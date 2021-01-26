74eb5986330468add7eab76179d1fb44101bb4cf
thinkverse <hallberg.kim@gmail.com>
2021-01-26 13:55:30 +0100

    Intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+  ],
+  darkMode: 'media',
+  theme: {
+    extend: {
+        fontFamily: {
+            sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+        }
+    },
+  },
+  variants: {
+    extend: {
+        opacity: ['disabled'],
+    },
+  },
+  plugins: [
+      require('@tailwindcss/forms'),
+  ],
+}
